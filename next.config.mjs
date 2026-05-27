8c9e9e7de285092950895198c3771a8ec9238db9
Omi-Das <omi06133@gmail.com>
2026-05-27 11:47:09 +0600

    Updated Profile page

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,22 @@ const nextConfig = {
   turbopack: {
     root: process.cwd(),
   },
+
+  images: {
+    domains: ["pngtree.com"],
+  },
 };
 
 export default nextConfig;
+
+// /** @type {import('next').NextConfig} */
+
+// const nextConfig = {
+//   reactCompiler: true,
+
+//   turbopack: {
+//     root: process.cwd(),
+//   },
+// };
+
+// export default nextConfig;
